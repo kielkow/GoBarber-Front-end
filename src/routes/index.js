8d667af396b69f3d0faf911499b83b9892b373bd
kielkow <matheuskiel@fiorifer.com.br>
2019-12-19 21:21:19 -0300

    Config routes

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Profile from '../pages/Profile';
+import Dashboard from '../pages/Dashboard';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Profile} />
+      <Route path="/profile" component={Dashboard} />
+    </Switch>
+  );
+}
