e1897a5d870019ad4ce2e736614d942598e2122b
kielkow <matheuskiel@fiorifer.com.br>
2019-12-21 22:45:51 -0300

    Private Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -14,8 +14,8 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" component={Profile} />
-      <Route path="/profile" component={Dashboard} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }
