a7fa9af62774bf0b0b3447a35d85b3138c1ed3d9
kielkow <matheuskiel@fiorifer.com.br>
2019-12-23 17:25:09 -0300

    Style Notifications

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Notifications from '~/components/Notifications';
+
 import logo from '~/assets/logoHeader.svg';
 
 import { Container, Content, Profile } from './styles';
@@ -15,6 +17,8 @@ export default function Header() {
         </nav>
 
         <aside>
+          <Notifications />
+
           <Profile>
             <div>
               <strong>Matheus Kielkowski</strong>
