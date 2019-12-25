5f7b929e19f1d7b6796814162c1fa261faf12990
kielkow <matheuskiel@fiorifer.com.br>
2019-12-25 18:03:03 -0300

    Data header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -8,6 +9,8 @@ import logo from '~/assets/logoHeader.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -21,12 +24,15 @@ export default function Header() {
 
           <Profile>
             <div>
-              <strong>Matheus Kielkowski</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">My Profile</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Matheus Kielkowski"
+              src={
+                profile.avatar ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
